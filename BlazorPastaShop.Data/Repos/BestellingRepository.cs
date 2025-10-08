c58b98755870fcf8be120dd2201dd615a1b63e3d
Sander De Sutter <sdsdesutter@live.be>
2025-10-08 14:51:40 +0200

    repos

---
+++ BlazorPastaShop.Data/Repos/BestellingRepository.cs
@@ -0,0 +1,27 @@
+﻿using BlazorPastaShop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPastaShop.Data.Repos
+{
+    public class BestellingRepository
+    {
+        private readonly BlazorPastaShopDbContext _context;
+
+        public BestellingRepository(BlazorPastaShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddBestellingAsync(Bestelling bestelling)
+        {
+            _context.Bestellingen.Add(bestelling);
+            await _context.SaveChangesAsync();
+        }
+
+        
+    }
+}
---
+++ BlazorPastaShop.Data/Repos/KlantRepository.cs
@@ -0,0 +1,24 @@
+﻿using BlazorPastaShop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPastaShop.Data.Repos
+{
+    public class KlantRepository
+    {
+        private readonly BlazorPastaShopDbContext _context;
+        public KlantRepository(BlazorPastaShopDbContext context) {
+            _context = context;
+        }
+
+        public async Task AddKlantAsync(Klant klant) {
+            _context.Klanten.Add(klant);
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
